727d8cb216b82688323cba71d1e6659636a48dff
qpaseo <qazcgik@gmail.com>
2025-06-23 20:37:17 +0900

    update :: 서비스, 컨트롤러 생성

--- src/main/java/com/example/row/controller/RowController.java
+++ src/main/java/com/example/row/controller/RowController.java
@@ -1,11 +1,25 @@
-//테스트
 package com.example.row.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
+import com.example.row.service.RowService;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/row")
 public class RowController {
+    private final RowService rowService;
 
+    @PostMapping("/create")
+    public String create(@RequestBody RowRequest dto) {
+        return rowService.create(dto) ? "created" : "fail";
+    }
+
+    @PostMapping("/update")
+    public String update(@RequestBody RowRequest dto) {
+        return rowService.update(dto) ? "updated" : "fail";
+    }
 }
--- src/main/java/com/example/row/service/RowService.java
+++ src/main/java/com/example/row/service/RowService.java
@@ -1,7 +1,35 @@
 package com.example.row.service;
 
+import com.example.row.dto.RowRequest;
+import com.example.row.entity.Row;
+import com.example.row.repository.RowRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RowService {
+    private final RowRepository rowRepository;
+
+    public String create(RowRequest dto) {
+        try {
+            Row row = new Row(dto); 
+            rowRepository.save(row);
+            return "저장에 성공하였습니다";
+        } catch (Exception e) {
+            return "저장에 성공하였습니다";
+        }
+    }
+
+    public String update(RowRequest dto) {
+        try {
+            Row row = rowRepository.findById(dto.getId()).orElse(null);
+            if (row == null) return false;
+            row.updateFromDto(dto);
+            rowRepository.save(row);
+            return "저장에 성공하였습니다";
+        } catch (Exception e) {
+            return "저장에 실패하였습니다";
+        }
+    }
 }
